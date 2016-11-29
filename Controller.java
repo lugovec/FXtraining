d3cb814df81d86298c376705c5568728fccc76fa
lugovec <lugovec-fox@yandex.ru>
2016-11-29 17:42:01 +0500

    Научился заполнять ChoiceBox и делать выбор в нём. Научился для событий использовать лямбда выражения и анонимные классы.

--- Controller.java
+++ Controller.java
@@ -1,10 +1,17 @@
 package sample;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 
 public class Controller {
@@ -15,6 +22,14 @@ public class Controller {
     @FXML
     private Button btn;
 
+    @FXML
+    ChoiceBox choiceBox;
+
+    @FXML
+    Circle circle;
+
+
+
     public void click(ActionEvent actionEvent){
 
         tbl.getColumns().add(new TableColumn<>("new column"));
@@ -23,6 +38,24 @@ public class Controller {
 
         tbl.prefWidth(400);
 
+        choiceBox.setItems(FXCollections.observableArrayList("red", "green", "orange", "yellow"));
+
+        Color[] colors = new Color[]{Color.RED, Color.GREEN, Color.ORANGE, Color.YELLOW};
+
+        choiceBox.getSelectionModel().selectedIndexProperty().addListener(
+                (ObservableValue<? extends Number> ov, Number old_val, Number new_val) -> {
+                    circle.setFill(colors[new_val.intValue()]);
+                });
+
+        /*то же самое через анонимный класс
+        choiceBox.getSelectionModel().selectedIndexProperty().addListener(
+                new ChangeListener<Number>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+
+                    }
+                }
+        );*/
 
     }
 
